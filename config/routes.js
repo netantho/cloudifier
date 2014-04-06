60075aecd1e1214f06aa13d08256c1ee52e7866f
Anthony Verez <averez@mozilla.com>
2014-04-05 22:20:57 -0700

    disable routing using blueprints, routes are now manually configured

--- config/routes.js
+++ config/routes.js
@@ -33,7 +33,6 @@ module.exports.routes = {
 
   // Custom routes here...
 
-  'get /api/v1/ec2regions/findAll': {response: 'notFound'},
   'get /api/v1/ec2/regions': {
     controller: 'Ec2regionsController',
     action: 'findAll'
