9f3133f0da6429617746a60f534d2b350b4a2394
Anthony Verez <netantho@gmail.com>
2014-05-31 22:43:09 -0700

    use mocha+chai with protractor

--- test/angular/e2e/scenarios.js
+++ test/angular/e2e/scenarios.js
@@ -2,12 +2,18 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
 describe('my app', function() {
 
   browser.get('/');
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).to.eventually.contain("/view1");
   });
 
 
@@ -19,8 +25,8 @@ describe('my app', function() {
 
 
     it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+      expect(element.all(by.css('[ng-view] p')).first().getText())
+        .to.eventually.contain('partial for view 1');
     });
 
   });
@@ -34,8 +40,8 @@ describe('my app', function() {
 
 
     it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+      expect(element.all(by.css('[ng-view] p')).first().getText())
+        .to.eventually.contain('partial for view 2');
     });
 
   });
